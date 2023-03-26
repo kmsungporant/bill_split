012a4d9f9af9550ad631336dd38ba6349b8070b7
kmsungporant <kmsungporant@gmail.com>
2023-03-26 00:48:03 -0400

    merge

--- frontend/lib/pages/PicturePage.dart
+++ frontend/lib/pages/PicturePage.dart
@@ -25,7 +25,7 @@ class ImagePick extends StatefulWidget {
 
 class _ImagePickerState extends State<ImagePick> {
   bool textScanning = false;
-
+  Map<int, String> lineMap = {};
   XFile? imageFile;
   String scannedText = "";
 
@@ -35,6 +35,12 @@ class _ImagePickerState extends State<ImagePick> {
         appBar: PreferredSize(
             preferredSize: const Size.fromHeight(60), child: BaseAppBar()),
         body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          if (!textScanning && imageFile == null)
+            Container(
+              width: 100,
+              height: 100,
+            ),
+          if (imageFile != null) Image.file(File(imageFile!.path)),
           const Text("Select your receipt",
               style: TextStyle(fontWeight: FontWeight.w900, fontSize: 32)),
           Container(
@@ -43,7 +49,8 @@ class _ImagePickerState extends State<ImagePick> {
               padding: const EdgeInsets.only(top: 10),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: Colors.brown,
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.brown,
                   shadowColor: Colors.black,
                   elevation: 10,
                   shape: RoundedRectangleBorder(
@@ -93,6 +100,50 @@ class _ImagePickerState extends State<ImagePick> {
       scannedText = "Error occured while scanning";
       setState(() {});
     }
+  }
+
+  void getRecognizedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final TextRecognizer textRecognizer =
+        TextRecognizer(script: TextRecognitionScript.latin);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+    await textRecognizer.close();
+    scannedText = "";
+    for (TextBlock block in recognizedText.blocks) {
+      // 'BOX COMBO'
+      for (TextLine line in block.lines) {
+        // if entry exists, will append to existing string. If not, will create new entry
+
+        lineMap.update(
+            handleCoordinateEstimation(lineMap, line.cornerPoints[0].y),
+            (value) => line.text + ";" + value,
+            ifAbsent: () => line.text);
+      }
+    }
+    for (String line in lineMap.values) {
+      if (line.toUpperCase().contains("TAX")) {
+        scannedText += line + "\n\n";
+        break;
+      } else if (RegExp(r'(\d+)\.(\d{2})', caseSensitive: false)
+          .hasMatch(line)) {
+        scannedText += line + "\n\n";
+      }
+    }
+    lineMap.clear();
+    textScanning = false;
+    setState(() {});
+  }
+
+  // Handles coordinate estimation for lineMap
+  int handleCoordinateEstimation(Map<int, String> lineMap, int y) {
+    for (int key in lineMap.keys) {
+      if ((key - y).abs() < 15) {
+        return key;
+      }
+    }
+
+    return y;
   }
 
   @override
