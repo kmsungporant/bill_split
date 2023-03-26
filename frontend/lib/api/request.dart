9202d4601223d08ab766019fcb14429e4e048da4
Dan Le <tle57@gmu.edu>
2023-03-26 06:49:45 -0400

    implement flaskAPI.py

--- frontend/lib/api/request.dart
+++ frontend/lib/api/request.dart
@@ -5,7 +5,7 @@ import '../models/Bill.dart';
 class request {
   static Future<http.Response> postBill(Bill bill) async {
     return await http.post(
-        Uri.parse("https://bestphat0409.pythonanywhere.com/parse_items"),
+        Uri.parse("http://127.0.0.1:5000/parse_items"), // https://bestphat0409.pythonanywhere.com
         body: bill.toString());
   }
 
